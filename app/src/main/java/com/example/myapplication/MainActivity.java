f5d63f2bed36189187d67c725449e1457d4a1ce1
Kevin Cedeño <joelcedeno612@gmail.com>
2023-12-20 10:33:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import WebService.Asynchtask;
+import WebService.WebService;
+
+public class MainActivity extends AppCompatActivity implements Asynchtask {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void clickLogin(View v){
+
+        Bundle bundle = this.getIntent().getExtras();
+        Map<String, String> datos = new HashMap<String, String>();
+        EditText txtusuario = findViewById(R.id.txtusuario);
+        EditText txtclave = findViewById(R.id.txtclave);
+        WebService ws= new WebService(
+                "https://revistas.uteq.edu.ec/ws/login.php?usr="
+                        + txtusuario.getText().toString() +
+                        "&pass=" + txtclave.getText().toString(),
+                datos,
+                MainActivity.this, MainActivity.this);
+        ws.execute("GET");
+
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+        TextView txtResp = findViewById(R.id.txtResp);
+        txtResp.setText(result);
+
+    }
+}
